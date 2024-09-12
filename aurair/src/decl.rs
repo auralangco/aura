dd3ca0c3c5cae599f960c553b548a9045fd87707
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-09-11 23:02:26 -0300

    aurair: feat: Add TyAssoc enum for associated members in type declarations

--- aurair/src/decl.rs
+++ aurair/src/decl.rs
@@ -32,12 +32,45 @@ pub struct ValDecl {
 #[derive(Debug, Clone, PartialEq)]
 pub struct TypeDecl {
     pub symbol: String,
-    pub ty: TypeExpr
+    pub ty: TypeExpr,
+    pub assoc: Vec<TyAssoc>,
 }
 
+/// Node for function declaration in Aura
+/// 
+/// # Example
+/// 
+/// ```norun
+/// fn sum(a Int, b Int) -> Int = a + b
+/// ```
 #[derive(Debug, Clone, PartialEq)]
 pub struct FnDecl {
     pub symbol: String,
     pub ty: FnType,
     pub body: FnBody
+}
+
+/// Node for associated members in a type declaration
+/// 
+/// Those members may be `val`, `fn` or `type`
+/// 
+/// # Example
+/// 
+/// ```norun
+/// type Coordinate = (x Int, y Int) {
+///     // Associated type
+///     type T = Int
+/// 
+///     // Associated value
+///     val origin Coordinate = Coordinate (x = 0, y = 0)
+///     
+///     // Associated function
+///     fn swap(self) -> Coordinate = Coordinate (x = self.y, y = self.x )
+/// }
+/// ```
+#[derive(Debug, Clone, PartialEq)]
+pub enum TyAssoc {
+    Val(ValDecl),
+    Fn(FnDecl),
+    Type(TypeDecl),
 }
